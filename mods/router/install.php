8f0ee75ba343816fb8d22a8689530297327b8478
GakmanAlexey <gakman@ya.ru>
2022-06-20 01:42:54 +0300

    0.001.010
    Добавлен Установщик Роутера

--- mods/router/install.php
+++ mods/router/install.php
@@ -2,6 +2,18 @@
 namespace Mods\Router;
 
 Class Install{
+    public function __construct(){
+        $sql_create = '
+        CREATE TABLE router (
+            id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+            PRIMARY KEY(id),
+            url VARCHAR(255) NOT NULL, 
+            class VARCHAR(255) NOT NULL,
+            funct VARCHAR(255) NOT NULL
+           )
+        ';
+        $sql = new \Mods\Sql\Sql;
+        $sql->db_connect->exec($sql_create);
+    } 
 
-    
 }
