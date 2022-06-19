63a2c9a1cfa71addef3b75378946fbc22c949087
GakmanAlexey <gakman@ya.ru>
2022-06-19 23:09:32 +0300

    0.001.006
    Добавлен в роутер формирователь путя

--- mods/router/router.php
+++ mods/router/router.php
@@ -5,11 +5,27 @@ Class Router{
     public $way = null;
 
     public function __construct(){
+        $this->start();
+        return;
+    }
+
+    public function start(){
         $this->way();
     }
+
+
+    
     public function way(){
-        $get = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];   
-        var_dump($get);     
+        //Получение адреса сайта
+        $get =  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; 
+        $array_adress_geter = explode("?", $get);
+        $array_adress = explode("/", $array_adress_geter[0]);
+        //удаление пустых элементов адреса
+        $array_get_clean = array_filter($array_adress);
+        unset ($array_get_clean[0]);
+
+        return $array_get_clean;
+        
     }
 
 }
