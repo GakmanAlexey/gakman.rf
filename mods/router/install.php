0bdc00be6dff7cf3f4996e3562e6e02bbab061f9
GakmanAlexey <gakman@ya.ru>
2022-06-19 23:29:44 +0300

    0.001.007
     Добавлен переадресация на установку

---
+++ mods/router/install.php
@@ -0,0 +1,7 @@
+<?php
+namespace Mods\Router;
+
+Class Install{
+
+    
+}
--- mods/router/router.php
+++ mods/router/router.php
@@ -3,18 +3,26 @@ namespace Mods\Router;
 
 Class Router{
     public $way = null;
+    public $static_list = null;
 
     public function __construct(){
+        $this->upload_static_list();
         $this->start();
         return;
     }
 
-    public function start(){
-        $this->way();
+    public function upload_static_list(){
+
+        $this->static_list["url"][] = "install";
+        $this->static_list["adress"][] = "install.php";
+        //----
     }
 
+    public function start(){
+        $this->way = $this->way();
+        $this->statik_list($this->way);
+    }
 
-    
     public function way(){
         //Получение адреса сайта
         $get =  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; 
@@ -24,8 +32,22 @@ Class Router{
         $array_get_clean = array_filter($array_adress);
         unset ($array_get_clean[0]);
 
-        return $array_get_clean;
-        
+        return $array_get_clean;        
     }
+
+    public function statik_list($way){
+        $timed = 0;
+        foreach($this->static_list["url"] as $url){
+            if($url == $way[1]){
+                include $this->static_list["adress"]["$timed"];
+                
+            }
+            $timed++;
+        }
+        //var_dump("<pre>",$way);
+        //var_dump("<pre>",$this->static_list);
+    }
+
+    
 
 }
